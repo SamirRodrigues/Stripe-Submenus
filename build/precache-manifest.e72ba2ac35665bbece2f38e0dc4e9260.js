da21863f4408a4d94bb6712e8ef4a8341357b57e
SamirRodrigues <emaildesamir@gmail.com>
2021-12-26 18:21:37 -0300

    deploy

---
+++ build/precache-manifest.e72ba2ac35665bbece2f38e0dc4e9260.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54eb0c144a08e39fcd532eecb85ac31",
+    "url": "/Stripe-Submenus/index.html"
+  },
+  {
+    "revision": "53069cd1f1d1090c6c5c",
+    "url": "/Stripe-Submenus/static/css/main.2346468d.chunk.css"
+  },
+  {
+    "revision": "70256363fdb63a326996",
+    "url": "/Stripe-Submenus/static/js/2.c11b1047.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stripe-Submenus/static/js/2.c11b1047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53069cd1f1d1090c6c5c",
+    "url": "/Stripe-Submenus/static/js/main.b1cc07f4.chunk.js"
+  },
+  {
+    "revision": "d0b4d5cba56e415be605",
+    "url": "/Stripe-Submenus/static/js/runtime-main.50619041.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/Stripe-Submenus/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/Stripe-Submenus/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/Stripe-Submenus/static/media/phone.58d7e3d6.svg"
+  }
+]);
